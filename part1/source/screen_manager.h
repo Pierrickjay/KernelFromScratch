1cb3ddeb892a9a6b002e6fae62f75577ca73c5be
Valentin Lepiller <valentin.l@galadrim.fr>
2025-02-23 17:02:13 +0100

    refactor: screen manager

--- part1/source/screen_manager.h
+++ part1/source/screen_manager.h
@@ -1,6 +1,8 @@
 #ifndef SCREEN_MANAGER_H
 #define SCREEN_MANAGER_H
 
+#include "utils.h"
+
 #define VGA_ADDRESS 0xB8000
 
 // Colors
@@ -17,13 +19,42 @@
 
 #define BG(color) (color << 4)
 
-// Window size
+// Window size (determines in boot.asm)
 #define H_WINDOW 25
 #define L_WINDOW 80
 
-extern unsigned short *terminal_buffer;
-extern unsigned int	   vga_index;
+#define SCREEN_BUFFER_SIZE (H_WINDOW * L_WINDOW * 2)
 
-void clear_screen(void);
+typedef struct s_character_cell
+{
+	unsigned char color;	 // private
+	unsigned char character; // private
+} t_character_cell;
+
+#define SCREEN_CELLS_SIZE (L_WINDOW * H_WINDOW)
+
+typedef struct s_desktop
+{
+	t_position		 cursor;					// private
+	t_character_cell cells[L_WINDOW][H_WINDOW]; // private
+} t_desktop;
+
+#define DESKTOP_COUNT 4
+
+typedef struct s_screen_context
+{
+	t_desktop	   desktops[DESKTOP_COUNT]; // private
+	unsigned int   desktop_index;			// private
+	unsigned char *vga_buffer;				// private
+} t_screen_context;
+
+extern t_screen_context screen_context;
+
+// Init functions
+void init_screen_context(t_screen_context *ctx);
+
+// Main functions
+void kfs_write_char(t_screen_context *ctx, unsigned char c, unsigned char color);
+void clear_screen(t_screen_context *ctx);
 
 #endif
--- part1/source/utils.h
+++ part1/source/utils.h
@@ -1,6 +1,16 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+#include <stddef.h>
+
+typedef struct s_position
+{
+	unsigned int x;
+	unsigned int y;
+} t_position;
+
+void init_position(t_position *pos);
+
 void itoa(int n, char *str);
 int	 intlen(int n);
 
