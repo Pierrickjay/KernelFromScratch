5fb505ad67cc3ffd8bfe96cd61ac49d73fb4495e
Valentin Lepiller <valentin.l@galadrim.fr>
2025-06-14 12:04:15 +0200

    refactor: keyboard_input.c

--- part1/source/keyboard_input.c
+++ part1/source/keyboard_input.c
@@ -6,40 +6,47 @@ static u8 special_keys		  = 0;
 static u8 left_shift_pressed  = 0;
 static u8 right_shift_pressed = 0;
 
+static int handle_special_key(unsigned char scancode)
+{
+	if (scancode == SCANCODE_LSHIFT) {
+		left_shift_pressed = 1;
+		special_keys |= SHIFT_PRESSED;
+		return 1;
+	}
+	else if (scancode == SCANCODE_RSHIFT) {
+		right_shift_pressed = 1;
+		special_keys |= SHIFT_PRESSED;
+		return 1;
+	}
+	else if (scancode == (SCANCODE_LSHIFT | 0x80)) {
+		left_shift_pressed = 0;
+		if (!right_shift_pressed)
+			special_keys &= ~SHIFT_PRESSED;
+		return 1;
+	}
+	else if (scancode == (SCANCODE_RSHIFT | 0x80)) {
+		right_shift_pressed = 0;
+		if (!left_shift_pressed)
+			special_keys &= ~SHIFT_PRESSED;
+		return 1;
+	}
+	else if (scancode == SCANCODE_CAPS_LOCK) {
+		special_keys ^= CAPS_LOCK;
+		return 1;
+	}
+
+	return 0;
+}
+
 void handle_keyboard_input(t_char_stacked_queue *queue)
 {
 	if (!queue_is_empty(queue)) {
 		unsigned char scancode = queue_pop(queue);
 
-		// Gestion des touches spéciales
-		if (scancode == SCANCODE_LSHIFT) {
-			left_shift_pressed = 1;
-			special_keys |= SHIFT_PRESSED;
-			return;
-		}
-		else if (scancode == SCANCODE_RSHIFT) {
-			right_shift_pressed = 1;
-			special_keys |= SHIFT_PRESSED;
-			return;
-		}
-		else if (scancode == (SCANCODE_LSHIFT | 0x80)) {
-			left_shift_pressed = 0;
-			if (!right_shift_pressed)
-				special_keys &= ~SHIFT_PRESSED;
-			return;
-		}
-		else if (scancode == (SCANCODE_RSHIFT | 0x80)) {
-			right_shift_pressed = 0;
-			if (!left_shift_pressed)
-				special_keys &= ~SHIFT_PRESSED;
-			return;
-		}
-		else if (scancode == SCANCODE_CAPS_LOCK) {
-			special_keys ^= CAPS_LOCK;
+		if (handle_special_key(scancode)) {
 			return;
 		}
 
-		// Conversion du scancode en caractère
 		char c = scancode_to_char(scancode, special_keys);
 		if (c) {
 			kfs_write_char(&screen_context, c);
