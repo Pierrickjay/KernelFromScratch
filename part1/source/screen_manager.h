96f37dc6ad380e172acc32a864326b224a226f21
Valentin Lepiller <valentin.l@galadrim.fr>
2025-07-06 12:05:45 +0200

    feat: switch desktop

--- part1/source/screen_manager.h
+++ part1/source/screen_manager.h
@@ -78,4 +78,6 @@ void		  set_color(t_screen_context *ctx, unsigned char color);
 
 void scroll_screen(t_screen_context *ctx);
 
+void change_desktop(t_screen_context *ctx, unsigned char desktop_index);
+
 #endif
